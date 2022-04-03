36840e066d94b21c4e18d0c02c99f5458bffbccb
32-Mimhakkani <mim.cse32@gmail.com>
2022-04-03 09:23:37 +0600

    qr code generator is ready

--- project1/app.js
+++ project1/app.js
@@ -5,12 +5,20 @@ const inputHandler =document.getElementById('qr_input')
 submitHandler.addEventListener('click',function(){
      const inputvalue =inputHandler.value
 
+     if(inputvalue!==''){ 
+
      //fetch qrcode apis
 
-     const qrcode = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputvalue}`
+     const qrcode = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputvalue}`;
 
+     document.getElementById('qr_image').style.opacity ='1'
+     document.getElementById('qr_image_div').classList.add("mystyle")
+
+     document.getElementById('qr_image').src =qrcode;
      
 
     inputHandler.value =" "
+
+    }
  
 })
